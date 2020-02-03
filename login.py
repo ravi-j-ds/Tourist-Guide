91c9a82bc980e9b7917e6e1a2b17b2016cc5cdde
Jarupula Ravi <60621113+ravi-j-ds@users.noreply.github.com>
2020-02-04 01:03:14 +0530

    login page Code

---
+++ login.py
@@ -0,0 +1,6 @@
+def login():
+  user-name = input("Please Enter user name")
+  pass-word = input("enter password: ")
+  re-pass-word = input(re-enter password: ")
+  country-name = input("Enter yoour country name")
+  
